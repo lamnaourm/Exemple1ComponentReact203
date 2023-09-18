d46c6c3d9c3444519bfeea545df5acc79a4e6e23
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-18 14:12:42 +0100

    exemple

---
+++ src/components/Bibliotheque.jsx
@@ -0,0 +1,8 @@
+
+function Bibliotheque() {
+    return <div>
+      <h1>Composant 2 Bibliotheque .... </h1>
+    </div>
+  }
+
+  export default Bibliotheque
---
+++ src/components/Livre.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+
+class Livre extends Component {
+
+    render(){
+      return <div>
+        <h1>Composant 1 .......</h1>
+      </div>
+    }
+  }
+
+  export default Livre;
